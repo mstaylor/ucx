1913f0b23841c2c0102a1e2a0c2bffee8fafac90
mstaylor <bud.staylor@gmail.com>
2023-12-31 17:34:41 -0500

    aws lambda holepunching support - partial

--- src/uct/tcp/tcp_iface.c
+++ src/uct/tcp/tcp_iface.c
@@ -772,7 +772,7 @@ static UCS_CLASS_INIT_FUNC(uct_tcp_iface_t, uct_md_h md, uct_worker_h worker,
                                             self->config.override_private_ip_address);
         if (status == UCS_OK && self->config.override_private_ip_address != NULL &&
             strlen(self->config.override_private_ip_address) > 0) {
-            status = ucs_netif_get_addr2(self->if_name,
+            status = ucs_netif_get_addr3(self->if_name,
                              (struct sockaddr*)&self->config.ifaddr,
                              (struct sockaddr*)&self->config.netmask,
                              self->config.override_private_ip_address);
