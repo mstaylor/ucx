1d21b7fd0131f2955b18235f51c9be4d2856be83
mstaylor <bud.staylor@gmail.com>
2024-01-26 14:27:41 -0500

    redis integration for hole punching

--- src/uct/tcp/tcp_cm.c
+++ src/uct/tcp/tcp_cm.c
@@ -897,7 +897,6 @@ ucs_status_t uct_tcp_cm_conn_start(uct_tcp_ep_t *ep)
         status = ucs_socket_setopt(ep->fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof timeout);
         if (status != UCS_OK) {
             ucs_warn("could NOT configure socket timeout");
-            return status;
         }
 
 
