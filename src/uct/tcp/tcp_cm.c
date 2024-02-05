baf4cdab043a20df177390702c34ab070c3c3b63
mstaylor <bud.staylor@gmail.com>
2024-02-05 16:03:53 -0500

    redis integration for hole punching

--- src/uct/tcp/tcp_cm.c
+++ src/uct/tcp/tcp_cm.c
@@ -925,6 +925,7 @@ ucs_status_t uct_tcp_cm_conn_start(uct_tcp_ep_t *ep)
             uct_tcp_cm_change_conn_state(ep, UCT_TCP_EP_CONN_STATE_CONNECTED);
         }
         ucs_warn("returning ucs_ok from cm endpoint and tcpunch");
+        uct_tcp_cm_conn_complete(ep);
         return UCS_OK;
     } else {
         //normal flow
