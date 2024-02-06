98b1bcfac727c490545079a2fe20df954254f6d8
mstaylor <bud.staylor@gmail.com>
2024-02-06 12:21:32 -0500

    redis integration for hole punching

--- src/uct/tcp/tcp_cm.c
+++ src/uct/tcp/tcp_cm.c
@@ -38,8 +38,9 @@ ucs_status_t ucs_netif_get_addr3(const char *if_name,
         ucs_warn("tcpunch enabled");
 
         //tomorrow - can tcpunch
+        ucs_warn("contacting rendezvous host: %s, port %i ", config->rendezvous_ip_address, config->rendezvous_port)
 
-        status = pair(peer_socket, &connect_addr, "testPairing", config->rendezvous_ip_address, config->rendezvous_port, 6000);
+        status = pair(peer_socket, &connect_addr, "test", config->rendezvous_ip_address, config->rendezvous_port, 60000);
 
 
         //set_sock_addr(NULL, &connect_addr, AF_INET, 0);
