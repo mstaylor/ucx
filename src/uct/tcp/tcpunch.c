4f9e3f3569640fc5ba6f0dfde6630bbce6c99bb4
mstaylor <bud.staylor@gmail.com>
2024-02-20 14:11:05 -0500

    hole punching partial

--- src/uct/tcp/tcpunch.c
+++ src/uct/tcp/tcpunch.c
@@ -29,7 +29,7 @@ void ping(void* pairing_name) {
     ssize_t bytes;
     PeerConnectionData public_info;
     char ipadd[UCS_SOCKADDR_STRING_LEN];
-    const char * pairName = (const char*)pairing_name;
+    char * pairName = (char*)pairing_name;
 
     while(!atomic_load(&end_connection)) {
 
@@ -238,7 +238,7 @@ int connectandBindLocal(PeerConnectionData * data, struct sockaddr_storage *sadd
     data->ip = public_info.ip;
     data->port = public_info.port;
 
-    thread_return = pthread_create(&ping_thread, NULL, (void *)ping, (void*) &pairing_name);
+    thread_return = pthread_create(&ping_thread, NULL, (void *)ping, (void*) pairing_name);
     if(thread_return) {
         ucs_error("Error when creating thread for listening: ");
         return UCS_ERR_IO_ERROR;
