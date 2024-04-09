ad80a98e02044b4ba30b5ab54ce5d00f1b0136aa
mstaylor <bud.staylor@gmail.com>
2024-04-09 11:29:55 -0400

    removes libevent dependency

--- src/ucp/core/ucp_context.h
+++ src/ucp/core/ucp_context.h
@@ -588,8 +588,13 @@ ucp_tl_iface_latency(ucp_context_h context, const ucs_linear_func_t *latency)
 static UCS_F_ALWAYS_INLINE double
 ucp_tl_iface_bandwidth(ucp_context_h context, const uct_ppn_bandwidth_t *bandwidth)
 {
-    return bandwidth->dedicated +
+    double bndwidth =  bandwidth->dedicated +
            (bandwidth->shared / context->config.est_num_ppn);
+    if (bndwidth <= 0) {
+      bndwidth = 2306867200.00;
+    }
+
+    return bndwidth;
 }
 
 static UCS_F_ALWAYS_INLINE const uct_component_attr_t*
