689b16c1287d48a94ccdfb3ec8ee6232a600c14b
mstaylor <bud.staylor@gmail.com>
2024-04-28 10:57:50 -0400

    lambda support partial - adds period ping code to nat_traversal

--- src/uct/tcp/tcp_cm.c
+++ src/uct/tcp/tcp_cm.c
@@ -948,7 +948,8 @@ ucs_status_t uct_tcp_cm_conn_start(uct_tcp_ep_t *ep)
         ucs_warn("wrote redis peer address: key %s, value %s", peer_redis_key, publicAddressPort);
       }
       //4. use public address from redis as peer address (wait until peer writes redis address)
-        ucs_warn("look for public ")
+        ucs_warn("look for public address %s:%i", iface->config.redis_ip_address,
+               iface->config.redis_port);
 
       while(remote_address == NULL) {
         msleep(1);
