4aead55177899140025fdb0184b656a3f1a20da9
mstaylor <bud.staylor@gmail.com>
2024-09-16 10:01:39 -0400

    updates override-remote-address3 to latest UCX to address issues with fargate execution and to realize UCX updates

--- src/uct/tcp/tcp_iface.c
+++ src/uct/tcp/tcp_iface.c
@@ -202,7 +202,7 @@ uct_tcp_iface_is_reachable_v2(const uct_iface_h tl_iface,
                               const uct_iface_is_reachable_params_t *params)
 {
     uct_tcp_iface_t *iface = ucs_derived_of(tl_iface, uct_tcp_iface_t);
-    uct_iface_local_addr_ns_t *local_addr_ns;
+    //uct_iface_local_addr_ns_t *local_addr_ns;
     uct_tcp_device_addr_t *tcp_dev_addr;
     int is_local_loopback, is_remote_loopback;
 
